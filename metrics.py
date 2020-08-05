61af9e1f2d310273c646f2b29220f903c3c91109
DESKTOP-GGHGGQQ\Praktikant04 <mivoplanic@gmail.com>
2020-08-05 09:53:04 +0200

    Izmenjeno slanje metrika

--- metrics.py
+++ metrics.py
@@ -1,10 +1,18 @@
 from app import Laptop
-from app import db
+from app import db, timeout
 from statsd import StatsClient
+from twisted.internet import task, reactor
 c = StatsClient(host="192.168.76.140",port=8125,prefix="laptopovi")
 
 db.connect()
-for laptop in Laptop.select():
-    c.incr(laptop.naziv.replace('\xa0', ' ') + ".comments", laptop.broj_komentara)
-    c.incr(laptop.naziv.replace('\xa0', ' ') + ".price", laptop.cena)
+def send_matrics():
+    for laptop in Laptop.select():
+        c.incr(laptop.naziv.replace('\xa0', ' ') + ".comments", laptop.broj_komentara)
+        c.incr(laptop.naziv.replace('\xa0', ' ') + ".price", laptop.cena)
+
+def sending():
+    loop = task.LoopingCall(send_matrics)
+    loop.start(timeout)
+    reactor.run()
+
 db.close()
