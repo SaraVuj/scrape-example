77f6da767b876bfda90b02f3efeb9ebe687482bb
DESKTOP-GGHGGQQ\Praktikant04 <mivoplanic@gmail.com>
2020-08-06 10:27:09 +0200

    #1 Refactoring

---
+++ config.py.example.py
@@ -0,0 +1,11 @@
+#Database settings
+#DB_FILE_NAME = 'database.db'
+
+#Scraping settings
+#URL = 'https://...'
+#USER_AGENT = 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'
+
+#StastDClient settings
+#STATSD_HOST = 'localhost'
+#STATSD_PORT = 8125
+#STATSD_PREFIX = 'prefix'
